36b0727455e0de5c237aace06b6ee5922dbe3564
michaellee17 <missing821017@yahoo.com.tw>
2023-08-22 17:27:30 +0800

    add:ts修正

--- src/store/counter.ts
+++ src/store/counter.ts
@@ -1,7 +1,6 @@
 import { ref, computed } from 'vue';
 import { defineStore } from 'pinia';
 
-// 使用composition API模式定义store
 export const useCounterStoreForSetup = defineStore('counterForSetup', () => {
   const count = ref<number>(1);
   const doubleCount = computed(() => count.value * 2);
