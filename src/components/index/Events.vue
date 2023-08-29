97a19e7c10930ff2f3b472021d12c965fcb5e97d
michaellee17 <missing821017@yahoo.com.tw>
2023-08-29 17:49:21 +0800

    add:首頁組件拆分

---
+++ src/components/index/Events.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+import Title from '@/components/widget/Title.vue'
+</script>
+<template>
+   <Title title="最新活動" />
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
 import Slider from '@/components/index/Slider.vue'
+import Search from '@/components/index/Search.vue'
+import Live from '@/components/index/Live.vue'
+import Events from '@/components/index/Events.vue'
+import Recommand from '@/components/index/Recommand.vue'
+import MainGod from '@/components/index/MainGod.vue'
+import Service from '@/components/index/Service.vue'
+import Product from '@/components/index/Product.vue'
 </script>
 <template>
     <Slider />
-    <h1 class="text-center bg-gray-100 p-5">Home</h1>
+    <Search />
+    <Live />
+    <Events />
+    <Recommand />
+    <MainGod />
+    <Service />
+    <Product />
 </template>
